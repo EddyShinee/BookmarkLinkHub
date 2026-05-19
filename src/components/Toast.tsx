18c3401cf136c8bc797147b13d31081b83040cd2
Eddy <thuanvd.uit@gmail.com>
2026-05-19 10:26:30 +0700

    Fix Get histories data

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -44,7 +44,7 @@ export default function Toast({ message, type, open, onClose, duration = 4000 }:
         };
 
   return (
-    <div className="fixed bottom-6 inset-x-0 z-[300] flex justify-center px-400 pointer-events-none">
+    <div className="fixed bottom-16 inset-x-0 z-[300] flex justify-center px-4 pointer-events-none">
       <div className="toast-pop-in pointer-events-auto relative max-w-[min(90vw,440px)] min-w-[min(100%,300px)]">
         {/* soft colored bloom behind glass */}
         <div
